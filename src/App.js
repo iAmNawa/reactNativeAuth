57b1d858acf6a7e295c09b01ef5a3f203b322475
Paul Borawski <paulborawski14@gmail.com>
2019-01-23 12:11:08 -0800

    adding firebase auth info

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import secrets from './secrets.js';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp(secrets);
+  }
+
   render() {
     return (
       <View>
