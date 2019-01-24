13e6a4d587c8fd10585191a464309605fc86ac0c
Paul Borawski <paulborawski14@gmail.com>
2019-01-23 22:29:23 -0800

    Log out button displaying when user is signed in

--- src/App.js
+++ src/App.js
@@ -1,20 +1,43 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, CardSection, Button } from './components/common';
 import secrets from './secrets.js';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = { loggedIn: null };
+
   componentWillMount() {
     firebase.initializeApp(secrets);
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    if (this.state.loggedIn) {
+      return (
+        <CardSection>
+          <Button>
+            Log Out
+          </Button>
+        </CardSection>
+      );
+    }
+    return <LoginForm />;
   }
 
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
