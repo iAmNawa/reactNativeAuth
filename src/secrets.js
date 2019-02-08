513e34b807c6550fe9701aad0ea6f1240d92e95e
Paul Borawski <paulborawski14@gmail.com>
2019-02-08 09:57:10 -0800

    minor changes

---
+++ src/secrets.js
@@ -0,0 +1,10 @@
+var secrets = {
+  apiKey: 'AIzaSyCxZ8-pGevFvGOZMRkcG7ETX8d41Ltvzxs',
+  authDomain: 'reactnativeauth-552f3.firebaseapp.com',
+  databaseURL: 'https://reactnativeauth-552f3.firebaseio.com',
+  projectId: 'reactnativeauth-552f3',
+  storageBucket: 'reactnativeauth-552f3.appspot.com',
+  messagingSenderId: '194542440496'
+}
+
+export default secrets;
